71761ae1b90f37cf7dc7cf38580ed0f44d78297b
waikeke <doe04147@naver.com>
2023-04-06 15:01:48 +0900

    add Homework#1 volume_calc.py

---
+++ volume_calc.py
@@ -0,0 +1,11 @@
+length = input("가로: ")
+width = input("세로: ")
+height = input("높이: ")
+
+length = float(length)
+width = float(width)
+height = float(height)
+
+volume = length*width*height
+
+print("박스의 부피 : ", volume)
